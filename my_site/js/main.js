b50a2f36406b8c45efe178c73e3379ffa0b72142
KlimovichD <53794157+KlimovichD@users.noreply.github.com>
2019-08-17 13:43:33 +0200

    Add files via upload

---
+++ my_site/js/main.js
@@ -0,0 +1,48 @@
+//Фиксация навигации сверху
+window.onload = function(){
+	window.onscroll = function showHeader(){
+		var header = document.querySelector('.header');
+
+		if(window.pageYOffset > 300){
+			header.classList.add('header_fixed');
+		}
+		else{
+			header.classList.remove('header_fixed');
+		}
+	}
+}
+
+
+//Всплывающее окно с картинкой
+$('#inline-popups').magnificPopup({
+  delegate: 'a',
+  removalDelay: 100, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeOpen: function() {
+       this.st.mainClass = this.st.el.attr('data-effect');
+    }
+  },
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
+
+$('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true,
+  closeOnContentClick:true,
+  closeBtnInside:false,
+  removalDelay: 100,
+});
+
+$(document).ready(function(){
+  $('a[href^="#"]').click(function(){
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 700);
+  });
+  $('.social-icon').hover(
+       function(){ $(this).addClass('animated jello') },
+       function(){ $(this).removeClass('animated jello') }
+);
+});
+
